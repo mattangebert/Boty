b91a2bedfe0afc81f3eeaf6451d59188ba31176c
mattan.gebert <mattan.gebert@flagbit.de>
2019-06-11 17:58:29 +0200

    [master] Add pw encoding and login

--- symfony/src/DataFixtures/AppFixtures.php
+++ symfony/src/DataFixtures/AppFixtures.php
@@ -5,9 +5,17 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+     {
+         $this->passwordEncoder = $passwordEncoder;
+     }
+
     public function load(ObjectManager $manager)
     {
         // $product = new Product();
@@ -16,8 +24,11 @@ class AppFixtures extends Fixture
         // create 3 user
         for ($i = 0; $i < 3; $i++) {
             $user =  new User();
-            $user->setEmail('user '.$i.'@test.de');
-            $user->setPassword('changeme123');
+            $user->setEmail('user_'.$i.'@test.de');
+            $user->setPassword($this->passwordEncoder->encodePassword(
+                $user,
+                'changeme123'
+            ));
             $user->setName('user '.$i);
             $manager->persist($user);
         }
--- symfony/src/Form/UserType.php
+++ symfony/src/Form/UserType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,8 +15,8 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('password')
+            ->add('email', EmailType::class)
+            ->add('password', PasswordType::class)
             ->add('name')
             ->add('gender')
             ->add('save', SubmitType::class)
